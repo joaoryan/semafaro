1a5ef0b79fbd32688fb23d56e147736c828a9efe
João Ryan Santos <sjoaoryan@gmail.com>
2023-05-15 11:39:52 -0300

    projeto

---
+++ Project/src/SemControle.java
@@ -0,0 +1,46 @@
+/*
+    Trabalho prático 3
+    Para fixar os conceitos fundamentais de Semáforos, crie um exemplo simples de aplicação Java que seja capaz de demonstrar:
+    O uso de uma Seção Crítica SEM controle;
+    O uso de uma Seção Crítica COM controle
+
+    Nome: João Ryan dos Santos
+    Matricula: 239
+
+    Logica usada para projeto foi utilizar um contador compartilhado por varias threads em que o contador e incrementado varias vezes, 
+    sem controle ocorrer uma condição de corrida (race condition), onde duas ou mais threads tentam acessar e modificar o contador ao mesmo tempo. 
+    Isso pode levar a resultados inconsistentes.
+
+    com controle usamos semáforo (Semaphore) para controlar o acesso à seção crítica, que é a operação de incrementar o contador. O semáforo é inicializado com uma permissão (1), 
+    o que permite que apenas uma thread acesse a seção crítica por vez. As outras threads aguardam a liberação do semáforo.
+*/
+
+public class SemControle {
+    private static int contador = 0;
+
+    public static void main(String[] args) {
+        // Criando várias threads que incrementam o contador simultaneamente
+        for (int i = 0; i < 5; i++) {
+            Thread thread = new Thread(() -> {
+                for (int j = 0; j < 100000; j++) {
+                    incrementar();
+                }
+            });
+            thread.start();
+        }
+
+        // Aguardando todas as threads terminarem
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        // Resultado esperado é 500000, mas pode variar a cada execução
+        System.out.println("Contador: " + contador);
+    }
+
+    public static void incrementar() {
+        contador++;
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # semafaro
+
+Para fixar os conceitos fundamentais de Semáforos, crie um exemplo simples de aplicação Java que seja capaz de demonstrar:
+O uso de uma Seção Crítica SEM controle;
+O uso de uma Seção Crítica COM controle
+
+Nome: João Ryan dos Santos
+Matricula: 239
+
+Logica usada para projeto foi utilizar um contador compartilhado por varias threads em que o contador e incrementado varias vezes,
+
+sem controle ocorrer uma condição de corrida (race condition), onde duas ou mais threads tentam acessar e modificar o contador ao mesmo tempo. 
+Isso pode levar a resultados inconsistentes.
+
+com controle usamos semáforo (Semaphore) para controlar o acesso à seção crítica, que é a operação de incrementar o contador. O semáforo é inicializado com uma permissão (1), 
+o que permite que apenas uma thread acesse a seção crítica por vez. As outras threads aguardam a liberação do semáforo.
